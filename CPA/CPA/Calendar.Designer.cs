41ce16fe79fc600b83e9caf76658d9df44a61e0a
cristi92b <cristi92b@gmail.com>
2014-11-07 14:16:56 +0200

    Change2

--- CPA/CPA/Calendar.Designer.cs
+++ CPA/CPA/Calendar.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(578, 300);
+            this.ClientSize = new System.Drawing.Size(412, 282);
             this.Name = "Calendar";
             this.Text = "Calendar";
             this.ResumeLayout(false);
