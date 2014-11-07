b4a7879e083511bd699198a4122c5878f386e13b
oneStep77 <adrian92_pl@yahoo.com>
2014-11-07 14:06:59 +0200

    Removed Form1

--- CPA/CPA/Program.cs
+++ CPA/CPA/Program.cs
@@ -16,7 +16,7 @@ namespace CPA
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new LoginForm());
         }
     }
 }
