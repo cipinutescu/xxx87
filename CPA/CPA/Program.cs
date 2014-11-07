408a412a73cc6485d46462b881886af588c63540
oneStep77 <adrian92_pl@yahoo.com>
2014-11-07 14:15:26 +0200

    checked item

--- CPA/CPA/Program.cs
+++ CPA/CPA/Program.cs
@@ -17,6 +17,8 @@ namespace CPA
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginForm());
+            int x = 10;
+            x++;
         }
     }
 }
